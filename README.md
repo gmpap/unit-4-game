21f3deb8e08dd9ff24b50a642821dfc8bc19a82b
George <dog8dog1@live.com>
2019-07-03 10:55:46 -0400

    This is the final working version of the game.  I cleaned a few things up and took care of the duplicate random numbers that were original showing up periodically. Now all numbers for each crystal are unique.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # unit-4-game
 crystal game
+
+Instructions to play the "crystal game".
+
+You will be given a random number at the start of the game.
+           
+There are four crystal below. By clicking on a crystal you will add a specific amount of
+points to your total.
+            
+The objective is to match your totals to the randomly generated number
+The value of each crystal is hidden from you, until you click on it.
+
+Note: Each time the game starts, a new set of numbers will be generated for each crystal...so Have Fun!!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,96 @@
 $(document).ready(function() {
 
+//This sets the variables for the random number, the users totals, wins, and losses.
+
     var randomNum = 0;
     var usersTotal = 0;
     var wins = 0;
     var losses = 0;
 
-//This sets the variables for the wins, losses and the computer random number
-// in the html code.
+//This function generates the random number for the computers number, starting from a vualue of 20,
+//and up to 80.
 
 function setgameRandnum(){
   randomNum = Math.floor((Math.random()*60)+20);
+  
 };
+
+//This function generates a random number between 1 and 12, and writes only unique values to the 
+//array 'crystalNumArray'.  It then loops through the array, and writes each value to
+//each crystals value.
+//
 
 function setCrystalvalues() {
-
-  for(let i = 0; i < 4; i++){
+  var crystalNumArray = []
+  while(crystalNumArray.length < 4){
+      var r = Math.floor(Math.random()*12) + 1;
+      if(crystalNumArray.indexOf(r) === -1) crystalNumArray.push(r);
+      
+  };
+  for(let i = 0; i < crystalNumArray.length; i++){
   
-  let crystalNum = Math.floor(Math.random()*12+1);
-  //console.log(crystalNum);
-  const crystal = $(".crystal").eq(i).attr("value", crystalNum);
+  let crystalNum = (crystalNumArray[i]);
+  console.log(crystalNum);                          
+  $(".crystal").eq(i).attr("value", crystalNum);
+
   };
 };
-  
+
+//Generates the computers number.  
 setgameRandnum();
 
-  $(".wins").text("Wins: " +wins);
-  $(".losses").text("Losses: " +losses);
+//Updates the display with the inital values.
+
+  $(".wins").text("Wins: " + wins);
+  $(".losses").text("Losses: " + losses);
   $(".randomNumber").text(randomNum);
   //$(".yourScore").attr("value", usersTotal);
   $(".yourScore").text(usersTotal);
   
- 
-//This sets a random number from 1 to 12 for each of the crystals,
-//and outputs the number to the html crystal objects value attribute.
-
+//Generates each crytals random number.
 setCrystalvalues();
-//This is the button click events for each crystal.  It outputs
+
+//This is the button click event for each crystal.  It outputs
 //to the display and increments the users total score.
 
 $(".crystal").on("click", function(){
   usersTotal = parseInt(usersTotal) + parseInt($(this).attr('value'));
 
-console.log($(this).attr('value'));
-console.log("yoursum: "+usersTotal);
+  console.log("your number: " + $(this).attr('value'));
+  console.log("yoursum: "+usersTotal);
 
-$(".yourScore").text(usersTotal);
+  $(".yourScore").text(usersTotal);
 
+//This checks if the users total is greater than the games number.
+//If so, then losses are incremented and the display is updated.
+//It then regenerates new crystal numbers and the computers number.
 
 if(usersTotal > randomNum) {
-  losses = losses +1;
-  console.log("losses: " +losses);
+  losses++;
+  console.log("losses: " + losses);
   $(".losses").text("Losses: " + losses);
-//call random numb genreator and update display
-
-usersTotal = 0;
-$(".yourScore").text(usersTotal);
-//reset crystal values
+  usersTotal = 0;
+  $(".yourScore").text(usersTotal);
+  alert("You went over!!  Try again.");
+//reset crystal values and games random number.
 setCrystalvalues();
 setgameRandnum();
+
+//updates the new random number to the display
 $(".randomNumber").text(randomNum);
 }
-
+//If the users total equals the computers number, then it increments the wins total,
+// regenerates new numbers for the crystals and the computers number, and updates the display.
 else{
   if(usersTotal == randomNum){
-    wins = wins +1;
+    wins++;
     $(".wins").text("Wins: " +wins);
       usersTotal = 0;
       setCrystalvalues();
       setgameRandnum();
-      $(".randomNumber").text(randomNum); 
+      $(".yourScore").text(usersTotal);
+      $(".randomNumber").text(randomNum);
+      alert("Congratulations!!!  You Won!!"); 
 
   }};
 
